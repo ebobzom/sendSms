896f0b07fa9cecd636d08e689e38a70f66cd4f69
Elijah Bobzom <ebobzom@gmail.com>
2019-03-26 15:12:33 +0100

    add signup route

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -6,6 +6,14 @@ appRouter.get('/', (req,res) => {
     res.render('login');
 });
 
+appRouter.post('/login', (req,res) =>{
+
+});
+
+appRouter.get('/signup', (req,res) => {
+    res.render('signup.ejs');
+})
+
 module.exports = {
     appRouter
 }
